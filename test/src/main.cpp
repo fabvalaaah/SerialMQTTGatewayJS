df39cf1b7b759a6f9f2cf650e553193b96acd304
Fabvalaaah <fabvalaaah@laposte.net>
2018-03-05 22:18:29 +0100

    Converting the Arduino test file into a (mighty) Atom project

---
+++ test/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+char command[50];
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (0 < Serial.available()) {
+        memset(command, '\0', 50);
+        Serial.readBytesUntil('\n', command, 50); // Input message from MQTT
+        /* TODO: DO WHATEVER */
+        Serial.println(command); // Output message to MQTT
+    }
+}
